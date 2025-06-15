71fe407690ac78ecbeb172f0f10daf406d759b71
Rob <rob@hitiq.co>
2025-06-16 10:53:38 +1200

    -

---
+++ components/tradingview/detailed.tsx
@@ -0,0 +1,55 @@
+// TradingViewWidget.jsx
+import React, {memo, useEffect, useRef} from 'react';
+
+type TradingViewWidgetProps = {
+  symbol?: string;
+  timezone?: string;
+};
+
+function TradingViewWidget({
+  symbol = 'GBPJPY.P',
+  timezone = 'New Zealand/Auckland',
+                           }: TradingViewWidgetProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(
+    () => {
+      if (!containerRef.current) return;
+
+      containerRef.current.innerHTML = '';
+
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "${symbol}",
+          "interval": 60,
+          "timezone": "${timezone}",
+          "theme": "light",
+          "style": "1",
+          "locale": "en",
+          "allow_symbol_change": true,
+          "hide_volume": true,
+          "support_host": "https://www.tradingview.com"
+        }`;
+      containerRef.current.appendChild(script);
+    },
+    []
+  );
+
+  return (
+    <div className="tradingview-widget-container" ref={containerRef} style={{height: "100%", width: "100%"}}>
+      <div className="tradingview-widget-container__widget" style={{height: "calc(100% - 32px)", width: "100%"}}></div>
+      <div className="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/"
+           rel="noopener nofollow"
+           target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
+      </div>
+    </div>
+  );
+}
+
+export default memo(TradingViewWidget);
